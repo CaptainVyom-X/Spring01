44d7df6a042ac990eab85b7085b33a6a81cea119
Subodh <welcome@DESKTOP-4BFJCBM>
2022-03-07 13:54:32 +0530

    @Autowired

---
+++ 15_IOC_A_StrategyDP_@Autowired/src/com/nt/comp/Flipkart.java
@@ -0,0 +1,41 @@
+package com.nt.comp;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public final class Flipkart {
+
+	@Autowired
+	@Qualifier("fx")
+	private Courier courier;
+
+	/*public Flipkart() {
+		System.out.println("Flipkart : 0-param constructor");
+	}
+	
+	public void setCourier(Courier courier) {
+		System.out.println("Flipkart.setCourier()");
+		this.courier = courier;
+	}*/
+
+	public String shopping(String[] items, float[] prices) {
+		int orderId = 0;
+		float billAmt = 0.0f;
+		String msg = null;
+
+		// generate order id
+		orderId = new Random().nextInt(10000);
+		// calculate bill amount
+		for (float p : prices)
+			billAmt += p;
+
+		// use courier
+		msg = courier.delivery(orderId);
+		return "Order for " + Arrays.toString(items) + " with prices " + Arrays.toString(prices) + " bill amt "
+				+ billAmt + " " + msg;
+	}
+
+}
